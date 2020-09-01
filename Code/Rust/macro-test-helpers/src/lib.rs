95a5a283f34e3d2c1f7b7e2d387cb31e62c096a7
Pieter <github@chesedo.me>
2020-09-01 18:01:39 +0000

    Add test helpers

---
+++ Code/Rust/macro-test-helpers/src/lib.rs
@@ -0,0 +1,106 @@
+use pretty_assertions::assert_eq;
+use std::{
+    fmt::Display,
+    io::Write,
+    process::{Command, Stdio},
+    str,
+};
+
+mod print;
+mod vec;
+mod vec_hand;
+
+/// Formats a [Display](https://doc.rust-lang.org/std/fmt/trait.Display.html) using `rustfmt`
+/// # Example
+/// ```
+/// use macro_test_helpers::reformat;
+///
+/// assert_eq!(
+///     reformat(&String::from("use std::{str,fmt};")),
+///     "use std::{fmt, str};\n"
+/// )
+/// ```
+pub fn reformat(text: &dyn Display) -> String {
+    let mut rustfmt = Command::new("rustfmt")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Failed to create command");
+    {
+        let stdin = rustfmt
+            .stdin
+            .as_mut()
+            .expect("Failed to create input stream");
+        stdin
+            .write_all(text.to_string().as_bytes())
+            .expect("Failed to write to input stream");
+    }
+    let output = rustfmt
+        .wait_with_output()
+        .expect("Format command did not end");
+    String::from_utf8(output.stdout).expect("Failed to convert output to string")
+}
+
+/// Calls `cargo expand` on a module and returns the output as a vector of lines
+fn expand_cli(module: &str) -> Vec<String> {
+    let output = Command::new("cargo")
+        .arg("expand")
+        .arg(module)
+        .output()
+        .expect("Failed to expand module");
+
+    let lines = str::from_utf8(&output.stdout[..])
+        .expect("Failed to convert output to string")
+        .lines();
+
+    lines
+        .skip(1)
+        .take_while(|l| *l != "}")
+        .map(|l| l[4..].to_owned())
+        .collect()
+}
+
+/// Expands a modules and check that it equals some expected string
+pub fn expand_eq(module: &str, expected: &str) {
+    assert_eq!(expand_cli(module), expected.lines().collect::<Vec<&str>>());
+}
+
+/// Checks that an expanded module equals the expanse of some `[module]_hand`
+pub fn expand(module: &str) {
+    assert_eq!(expand_cli(module), expand_cli(&format!("{}_hand", module)));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn reformat_simple() {
+        assert_eq!(
+            reformat(&String::from("trait Test< SomeType > { }")),
+            "trait Test<SomeType> {}\n"
+        );
+    }
+
+    #[test]
+    fn expand_eq_print() {
+        expand_eq(
+            "print",
+            r#"fn test() {
+    ::std::io::_print(::core::fmt::Arguments::new_v1(
+        &["Test"],
+        &match () {
+            () => [],
+        },
+    ));
+}
+"#,
+        );
+    }
+
+    #[test]
+    fn expand_vec() {
+        expand("vec");
+    }
+}
---
+++ Code/Rust/macro-test-helpers/src/vec.rs
@@ -0,0 +1,15 @@
+macro_rules! vec {
+    ( $( $x:expr ),* ) => {
+        {
+            let mut temp_vec = Vec::new();
+            $(
+                temp_vec.push($x);
+            )*
+            temp_vec
+        }
+    };
+}
+
+fn Values() -> Vec<i32> {
+    vec![1, 2, 3]
+}
