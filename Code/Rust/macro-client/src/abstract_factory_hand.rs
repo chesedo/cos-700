5856dd021f9c410d616abe88d4fba1e4d035bd0c
Pieter <github@chesedo.me>
2020-09-03 18:43:32 +0000

    Expand client tests for Abstract Factory

--- Code/Rust/macro-client/src/abstract_factory_hand.rs
+++ Code/Rust/macro-client/src/abstract_factory_hand.rs
@@ -1,7 +1,63 @@
 use macro_patterns::abstract_factory;
 
+trait Element {
+    fn name(&self) -> &str;
+}
+
 struct Button {}
+
+impl Element for Button {
+    fn name(&self) -> &str {
+        "Button"
+    }
+}
+
 struct Window {}
-trait Factory<T> {}
+
+impl Element for Window {
+    fn name(&self) -> &str {
+        "Window"
+    }
+}
+
+trait Factory<T: Element> {
+    fn create(&self) -> T;
+}
 
 trait Gui: Factory<Button> + Factory<Window> {}
+
+struct KDE {}
+
+impl Gui for KDE {}
+
+impl Factory<Button> for KDE {
+    fn create(&self) -> Button {
+        Button {}
+    }
+}
+impl Factory<Window> for KDE {
+    fn create(&self) -> Window {
+        Window {}
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn button_factory() {
+        let factory = KDE {};
+        let actual: Button = factory.create();
+
+        assert_eq!(actual.name(), "Button");
+    }
+
+    #[test]
+    fn window_factory() {
+        let factory = KDE {};
+        let actual: Window = factory.create();
+
+        assert_eq!(actual.name(), "Window");
+    }
+}
--- Code/Rust/macro-client/src/abstract_factory_r.rs
+++ Code/Rust/macro-client/src/abstract_factory_r.rs
@@ -1,8 +1,27 @@
 use crate::abstract_factory_r;
 
-trait Factory<T> {}
+trait Factory<T: Element> {
+    fn create(&self) -> T;
+}
+trait Element {
+    fn name(&self) -> &str;
+}
+
 struct Button {}
+
+impl Element for Button {
+    fn name(&self) -> &str {
+        "Button"
+    }
+}
+
 struct Window {}
+
+impl Element for Window {
+    fn name(&self) -> &str {
+        "Window"
+    }
+}
 
 abstract_factory_r!(Gui, Factory, Button, Window);
 
@@ -10,5 +29,34 @@ struct KDE {}
 
 impl Gui for KDE {}
 
-impl Factory<Button> for KDE {}
-impl Factory<Window> for KDE {}
+impl Factory<Button> for KDE {
+    fn create(&self) -> Button {
+        Button {}
+    }
+}
+impl Factory<Window> for KDE {
+    fn create(&self) -> Window {
+        Window {}
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn button_factory() {
+        let factory = KDE {};
+        let actual: Button = factory.create();
+
+        assert_eq!(actual.name(), "Button");
+    }
+
+    #[test]
+    fn window_factory() {
+        let factory = KDE {};
+        let actual: Window = factory.create();
+
+        assert_eq!(actual.name(), "Window");
+    }
+}
