80de155b0667785ebd296b8d5dfe4943b5f6f201
Pieter <github@chesedo.me>
2020-09-28 06:01:18 +0000

    Break apart tests

--- Code/Rust/macro-patterns-dec/tests/tests.rs
+++ Code/Rust/macro-patterns-dec/tests/tests.rs
@@ -1,4 +1,14 @@
 #[test]
-pub fn pass() {
-    macrotest::expand("tests/expand/**/*.rs");
+pub fn pass_helpers() {
+    macrotest::expand("tests/expand/helpers/*.rs");
+}
+
+#[test]
+pub fn pass_abstract_factory() {
+    macrotest::expand("tests/expand/abstract_factory/*.rs");
+}
+
+#[test]
+pub fn pass_visitor() {
+    macrotest::expand("tests/expand/visitor/*.rs");
 }
