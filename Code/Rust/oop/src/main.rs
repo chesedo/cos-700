6344c6f96760783d1b48edd0cc3e92c35182357b
Pieter <github@chesedo.me>
2020-09-29 05:26:09 +0000

    Report on OOP

---
+++ Code/Rust/oop/src/main.rs
@@ -0,0 +1,40 @@
+struct Foo {
+    pub name: String,
+    age: u8,
+    gender: Gender,
+}
+
+impl Foo {
+    pub fn get_age(&self) -> u8 {
+        self.age
+    }
+
+    pub fn have_birthday(&mut self) {
+        self.age += 1;
+    }
+
+    fn have_burial(self) {
+        unimplemented!();
+    } // `self` will be dropped here
+}
+
+fn main() {
+    let mut bar = Foo {
+        name: String::from("bar"),
+        age: 1,
+        gender: Gender::Male,
+    };
+
+    bar.have_birthday();
+
+    // Party was crazy
+    bar.have_burial();
+
+    println!("Bar was {} years old", bar.get_age()); // borrow of moved value: 'bar'
+}
+
+enum Gender {
+    Male,
+    Female,
+    Unknown,
+}
