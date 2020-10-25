4f97f4c28989f04f3f72e9822fa4252e638b5077
Pieter <github@chesedo.me>
2020-10-25 17:52:27 +0000

    Meeting changes

--- Code/Rust/Metaprogramming/library/src/lib.highlighted.rs
+++ Code/Rust/Metaprogramming/library/src/lib.highlighted.rs
@@ -11,9 +11,11 @@ use quote::quote;
 
 #[proc_macro_derive(|\functionh{GetType}|)]
 pub fn derive_answer_fn(|\contexth{tokens: TokenStream}|) -> |\outputh{TokenStream}| {
+    // Parse the context tokens to a DeriveInput syntax tree
     let context = parse_macro_input!(tokens as DeriveInput);
+    // Get the type name from the syntax tree
     let name = &context.ident;
-
+    // Construct the output using the quote macro
     let output = quote! {
         impl GetType for |\#|name {
             fn get_type(&self) -> String {
@@ -22,10 +24,10 @@ pub fn derive_answer_fn(|\contexth{tokens: TokenStream}|) -> |\outputh{TokenStre
         }
     };
 
-    output.into()
+    output.into() // Convert the output to a TokenStream
 }
 
 #[proc_macro_attribute]
-pub fn |\functionh{reflect\_two}|(|\inputh{attr: TokenStream}|, |\contexth{item: TokenStream}|) -> |\outputh{TokenStream}| {
-    item
+pub fn |\functionh{reflect\_two}|(|\inputh{input: TokenStream}|, |\contexth{context: TokenStream}|) -> |\outputh{TokenStream}| {
+    context
 }
--- Code/Rust/Metaprogramming/library/src/lib.rs
+++ Code/Rust/Metaprogramming/library/src/lib.rs
@@ -11,9 +11,11 @@ use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(GetType)]
 pub fn derive_answer_fn(tokens: TokenStream) -> TokenStream {
+    // Parse the context tokens to a DeriveInput syntax tree
     let context = parse_macro_input!(tokens as DeriveInput);
+    // Get the type name from the syntax tree
     let name = &context.ident;
-
+    // Construct the output using the quote macro
     let output = quote! {
         impl GetType for #name {
             fn get_type(&self) -> String {
@@ -22,10 +24,10 @@ pub fn derive_answer_fn(tokens: TokenStream) -> TokenStream {
         }
     };
 
-    output.into()
+    output.into() // Convert the output to a TokenStream
 }
 
 #[proc_macro_attribute]
-pub fn reflect_two(attr: TokenStream, item: TokenStream) -> TokenStream {
-    item
+pub fn reflect_two(input: TokenStream, context: TokenStream) -> TokenStream {
+    context
 }
