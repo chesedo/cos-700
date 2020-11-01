064fe62231e997e03365769318ca488663effe5e
Pieter <github@chesedo.me>
2020-11-01 13:42:19 +0000

    Make SimpleType

--- Code/Rust/macro-lib/src/lib.rs
+++ Code/Rust/macro-lib/src/lib.rs
@@ -1,7 +1,10 @@
 pub mod key_value;
 pub mod options_attribute;
-pub mod rich_type;
+mod rich_type;
+mod simple_type;
 pub mod token_stream_utils;
 pub mod trait_specifier;
 
+pub use rich_type::RichType;
+pub use simple_type::SimpleType;
 // TODO: consider if some Punctuated::parse_terminated should no be Punctuated::parse_seperated_nonempty
---
+++ Code/Rust/macro-lib/src/simple_type.rs
@@ -0,0 +1,68 @@
+use proc_macro2::TokenStream;
+use quote::ToTokens;
+use syn::parse::{Parse, ParseStream, Result};
+use syn::{Ident, Token};
+
+/// Holds a simple type that is optionally annotated as `dyn`
+#[derive(Eq, PartialEq, Debug)]
+pub struct SimpleType {
+    pub dyn_token: Option<Token![dyn]>,
+    pub ident: Ident,
+}
+
+/// Make SimpleType parsable from token stream
+impl Parse for SimpleType {
+    fn parse(input: ParseStream) -> Result<Self> {
+        Ok(SimpleType {
+            dyn_token: input.parse()?,
+            ident: input.parse()?,
+        })
+    }
+}
+
+/// Turn SimpleType back into a token stream
+impl ToTokens for SimpleType {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        self.dyn_token.to_tokens(tokens);
+        self.ident.to_tokens(tokens);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+    use syn::parse_str;
+
+    type Result = std::result::Result<(), Box<dyn std::error::Error>>;
+
+    #[test]
+    fn parse() -> Result {
+        let actual: SimpleType = parse_str("dyn Button")?;
+        let expected = SimpleType {
+            dyn_token: Some(Default::default()),
+            ident: parse_str("Button")?,
+        };
+
+        assert_eq!(actual, expected);
+        Ok(())
+    }
+
+    #[test]
+    fn parse_without_dyn() -> Result {
+        let actual: SimpleType = parse_str("Button")?;
+        let expected = SimpleType {
+            dyn_token: None,
+            ident: parse_str("Button")?,
+        };
+
+        assert_eq!(actual, expected);
+        Ok(())
+    }
+
+    #[test]
+    #[should_panic(expected = "expected identifier")]
+    fn missing_type() {
+        parse_str::<SimpleType>("dyn").unwrap();
+    }
+}
