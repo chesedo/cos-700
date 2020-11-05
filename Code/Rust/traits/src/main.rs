c8db23d9987fe8bbde8a758cdfa7225ebc230385
Pieter <github@chesedo.me>
2020-11-05 12:20:29 +0000

    Trim Rust section

--- Code/Rust/traits/src/main.rs
+++ Code/Rust/traits/src/main.rs
@@ -1,5 +1,3 @@
-use std::fmt::Display;
-
 trait Show {
     fn show(&self) -> String;
 
@@ -11,20 +9,6 @@ trait Show {
 
 fn work<T: Show>(object: T) {
     println!("{}", object.show());
-}
-
-fn complex<T: Show + Display + PartialEq<U>, U: Display>(first: T, second: U) {
-    if first == second {
-        println!("{}({}) is equals {}", first, first.show(), second);
-    }
-}
-
-fn complex_where<T, U>(first: T, second: U)
-where
-    T: Show + Display + PartialEq<U>,
-    U: Display,
-{
-    // Same as complex with easier to read trait bounds
 }
 
 struct Tester {}
@@ -40,6 +24,12 @@ fn work_tester(object: Tester) {
 }
 fn work_string(object: String) {
     println!("{}", object.show());
+}
+
+impl Show for String {
+    fn show(&self) -> String {
+        self.clone()
+    }
 }
 
 fn main() {
